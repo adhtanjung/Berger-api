965f538199f373a19864c332b9defcb1c6a5e5c2
adhtanjung <adhtanjung@gmail.com>
2021-02-02 10:46:48 +0700

    heroku

--- database/index.js
+++ database/index.js
@@ -1,18 +1,18 @@
 const mysql = require("mysql");
 
-// const db = mysql.createConnection({
-// 	host: "db4free.net",
-// 	user: "berger",
-// 	password: "Asd12345",
-// 	database: "berger",
-// 	port: 3306,
-// });
 const db = mysql.createConnection({
-	host: "localhost",
-	user: "adhitanjung",
-	password: "asd123",
+	host: "db4free.net",
+	user: "bergerdb",
+	password: "Asd12345",
 	database: "berger",
 	port: 3306,
 });
+// const db = mysql.createConnection({
+// 	host: "localhost",
+// 	user: "adhitanjung",
+// 	password: "asd123",
+// 	database: "berger",
+// 	port: 3306,
+// });
 
 module.exports = db;
