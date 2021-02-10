3c9166f509129f51947004846d4dae45768fa991
adhtanjung <adhtanjung@gmail.com>
2021-02-10 20:15:35 +0700

    heroku

--- database/index.js
+++ database/index.js
@@ -8,12 +8,12 @@ const db = mysql.createConnection({
 	database: "berger",
 	port: 3306,
 });
-const db = mysql.createConnection({
-	host: process.env.DB_HOST,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DB_DATABASE,
-});
+// const db = mysql.createConnection({
+// 	host: process.env.DB_HOST,
+// 	user: process.env.DB_USER,
+// 	password: process.env.DB_PASSWORD,
+// 	database: process.env.DB_DATABASE,
+// });
 
 // const db = mysql.createConnection({
 // 	host: "localhost",
