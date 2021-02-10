273faa65ddbbef2b148a74db4abf4c488b081508
adhtanjung <adhtanjung@gmail.com>
2021-02-10 17:03:12 +0700

    heroku

--- database/index.js
+++ database/index.js
@@ -1,13 +1,18 @@
 const mysql = require("mysql");
 const { MongoClient, ObjectID } = require("mongodb");
-const util = require("util");
-require("dotenv/config");
+
+const db = mysql.createConnection({
+	host: "db4free.net",
+	user: "bergerdb",
+	password: "Asd12345",
+	database: "berger",
+	port: 3306,
+});
 const db = mysql.createConnection({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_DATABASE,
-	port: 3306,
 });
 
 // const db = mysql.createConnection({
@@ -17,9 +22,8 @@ const db = mysql.createConnection({
 // 	database: "berger",
 // 	port: 3306,
 // });
-const query = util.promisify(db.query).bind(db);
 
 const url =
 	"mongodb+srv://adhtanjung:Asd12345@bergercluster.lnuli.mongodb.net/test?retryWrites=true&w=majority";
 
-module.exports = { query, mongo: { MongoClient, ObjectID, url } };
+module.exports = { db, mongo: { MongoClient, ObjectID, url } };
